7b8bdd7dcf2021030c2d05a602283f2510aab515
Irfan Alif Ahmadi <irfanalifahmd@gmail.com>
2026-09-24 19:58:33 +0700

    redesign ui 1.1

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,22 +4,25 @@
     <h1 class="mt-3 mb-4">{{ $title }}</h1>
     @if ($posts->count())
         <div class="card mb-3">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h3 class="card-title">{{ $posts[0]->title }}</h3>
+            <img src="{{ asset('img/ppeva.webp') }}" class="card-img-top" alt="EVA-01">
+            <div class="card-body text-center">
+                <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}"
+                        class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
                 <p>By <a href="/authors/{{ $posts[0]->user->username }}"
                         class="text-decoration-none">{{ $posts[0]->user->name }}</a>
                     in
                     <a href="/categories/{{ $posts[0]->category->slug }}"
-                        class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+                        class="text-decoration-none">{{ $posts[0]->category->name }}</a> •
+                    <small class="text-body-secondary">{{ $posts[0]->created_at->diffForHumans() }}</small>
                 </p>
                 <p class="card-text">{{ $posts[0]->excerpt }}</p>
-                <p class="card-text"><small class="text-body-secondary">{{ $posts[0]->created_at->diffForHumans() }}</small>
                 </p>
+                <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
             </div>
         </div>
     @else
     @endif
+
     @foreach ($posts as $post)
         <article class="mb-5 border-bottom pb-4">
             <h2>
